3c1e4de08f4be41dfd864928c618093a7ea4b9fb
wangbin <wangbin_qwer@163.com>
2025-03-30 23:14:54 +0800

    test(): use org.apache.commons

--- list/src/main/java/splitList.java
+++ list/src/main/java/splitList.java
@@ -1,15 +1,20 @@
-import com.google.common.collect.Lists;
+
+import org.apache.commons.collections4.ListUtils;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class splitList {
     public static void main(String[] args) {
-        List<String> tempList = Arrays.asList("水星", "金星", "地球", "火星",
+        /*List<String> tempList = Arrays.asList("水星", "金星", "地球", "火星",
                 "冥王星", "土星", "天王星", "海王星", "冥王星", "木星");
 // size 是把集合拆分的大小，size 为表示拆分成拆分的集合大小为3，
 // 后面不足3的有多少算多少
         List<List<String>> partition = Lists.partition(tempList, 3);
+        System.out.println(partition);*/
+
+        List<String> tempList = Arrays.asList("水星","金星","地球","火星","冥王星","土星","天王星","海王星","冥王星","木星");
+        List<List<String>> partition = ListUtils.partition(tempList, 6);
         System.out.println(partition);
     }
 }
--- list/src/test/java/list.java
+++ list/src/test/java/list.java
@@ -1,4 +1,3 @@
-import org.apache.commons.collections.ListUtils;
 import org.junit.Test;
 import com.google.common.collect.Lists;
 
@@ -35,6 +34,17 @@ public class list {
         List<Integer> lastPartition = subSets.get(2);
         List<Integer> expectedLastPartition = Lists.<Integer> newArrayList(7, 8);
         assertThat(subSets.size(), equalTo(4));
+        assertThat(lastPartition, equalTo(expectedLastPartition));
+    }
+
+    @Test
+    public void givenList_whenParitioningIntoNSublists_thenCorrect() {
+        List<Integer> intList = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8);
+        List<List<Integer>> subSets = Lists.partition(intList, 3);
+
+        List<Integer> lastPartition = subSets.get(2);
+        List<Integer> expectedLastPartition = Lists.<Integer> newArrayList(7, 8);
+        assertThat(subSets.size(), equalTo(3));
         assertThat(lastPartition, equalTo(expectedLastPartition));
     }
 }
